8d535ad02123b29c8df6da6ae127733243f6e1fb
Peter Plamondon <394133+peterpla@users.noreply.github.com>
2020-05-12 17:48:44 -0700

    fix: support env var TIMELAPSE_PATH, TIMELAPSE_PORT

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/spf13/pflag v1.0.3
+	github.com/spf13/viper v1.7.0
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
 
--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
-import "github.com/go-playground/validator"
+import (
+	"log"
+	"os"
 
+	"github.com/go-playground/validator"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+// Config holds application-wide configuration info
+type Config struct {
+	path string
+	port string
+}
+
+var config Config
 var validate *validator.Validate
 
 func main() {
-	// TODO: env var: TIMELAPSE_DEFINITIONS (path to timelapse.json file)
-	// TODO: env var: TIMELAPSE_PORT (HTTP port to listen on)
+	c := Config{}
+	c.Load()
+	log.Printf("main: path=%q, port=%q\n", c.path, c.port)
 
-	// TODO: command line arguments
-
-	validate = validator.New()
+	// validate = validator.New()
 
 	// TODO: read Timelapse Definitions (TLDef) master list
 	// TODO: create Go routine to handle each TLDef
@@ -18,6 +31,32 @@ func main() {
 
 	// TODO: handler for webform to enter new TLDef
 	// TODO: handler on webform submit adds new TLDef to master list
+}
+
+// Load populates Config with application configuration info
+func (c *Config) Load() {
+
+	pflag.StringVar(&c.path, "path", "./", "path to folder containing timelapse.json")
+	pflag.StringVar(&c.port, "port", "8099", "HTTP port to listen on")
+	var help bool
+	pflag.BoolVarP(&help, "help", "h", false, "show usage information")
+	pflag.Parse()
+
+	if help {
+		pflag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	viper.BindPFlag("path", pflag.Lookup("path"))
+	viper.BindPFlag("port", pflag.Lookup("port"))
+
+	viper.SetEnvPrefix("timelapse")
+	viper.AutomaticEnv()
+	viper.BindEnv("path") // treats as upper-cased SerEnvPrefix value + "_" + upper-cased "path" (BindEnv argument)
+	viper.BindEnv("port")
+
+	c.path = viper.GetString("path")
+	c.port = viper.GetString("port")
 }
 
 // TLDef represents a Timelapse capture definition
